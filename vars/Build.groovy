a9267fee46a85f1d78bf10175a5077b3537ccde8
Anton_Tarianyk <Anton_Tarianyk@epam.com>
2020-04-15 12:57:47 +0300

    asd
    Change-Id: Id329581d7f340e420da47a96fbca3af96a4d32f1

--- vars/Build.groovy
+++ vars/Build.groovy
@@ -60,6 +60,8 @@ def call() {
                 def codebaseBranch = getCodebaseBranch(context.codebase.config.codebase_branch, context.git.branch)
                 def build = codebaseBranch.build_number.toInteger()
                 def version = codebaseBranch.version
+                script.println("[JENKINS][DEBUG] version version - ${version}")
+                context.script.println("[JENKINS][DEBUG] version version - ${version}")
                 def currentBuildNumber = ++build
                 def isReleaseBranch = codebaseBranch.release
 
