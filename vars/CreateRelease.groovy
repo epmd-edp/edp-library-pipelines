7d4ade8d23341b4e6619d22aa4312766ef642a9a
Alexander Morozov <alexander_morozov@epam.com>
2020-05-05 14:11:25 +0300

    [EPMDEDP-5068] Add capability to set trigger job parameters via env variables
    Change-Id: I42f00ef442c6f4d786836f28230bdd1eb8dca46e

--- vars/CreateRelease.groovy
+++ vars/CreateRelease.groovy
@@ -54,9 +54,10 @@ def call() {
         context.workDir = new File("/tmp/${RandomStringUtils.random(10, true, true)}")
         context.workDir.deleteDir()
 
-        context.triggerJobName = "job-provisions/${context.codebase.config.jobProvisioning}"
-        context.triggerJobWait = true
-        context.triggerJobParameters = [
+        context.job.triggerJobName = "job-provisions/${context.codebase.config.jobProvisioning}"
+        context.job.triggerJobWait = true
+        context.job.triggerJobPropogate = true
+        context.job.triggerJobParameters = [
                 string(name: 'PARAM', value: "true"),
                 string(name: 'NAME', value: "${context.codebase.config.name}"),
                 string(name: 'TYPE', value: "${context.codebase.config.type}"),
@@ -67,6 +68,8 @@ def call() {
                 string(name: 'GIT_CREDENTIALS_ID', value: "${context.git.credentialsId}"),
                 string(name: 'REPOSITORY_PATH', value: "${context.job.getParameterValue("REPOSITORY_PATH")}"),
         ]
+
+        println(context.job.triggerJobParameters)
 
         context.job.stages.each() { stage ->
             if (stage instanceof ArrayList) {
