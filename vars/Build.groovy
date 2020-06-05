81aa2743675bea8f8e2123ba632a1c0803a9160d
Mykola_Marusenko <mykola_marusenko@epam.com>
2020-06-05 14:12:28 +0300

    [EPMDEDP-5202] Add Chartmuseum route
    Change-Id: I775430d52dc2e91a6d0ad0df394245f34a8bb072

--- vars/Build.groovy
+++ vars/Build.groovy
@@ -18,6 +18,7 @@ import com.epam.edp.JobType
 import com.epam.edp.GitInfo
 import com.epam.edp.Nexus
 import com.epam.edp.Sonar
+import com.epam.edp.Chartmuseum
 import com.epam.edp.platform.PlatformType
 import com.epam.edp.platform.PlatformFactory
 import com.epam.edp.buildtool.BuildToolFactory
