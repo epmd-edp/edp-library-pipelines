b1f429864041a87479bdf5879947f5192b24ad36
Mykola_Marusenko <mykola_marusenko@epam.com>
2020-06-05 14:12:28 +0300

    [EPMDEDP-5202] Add Chartmuseum route
    Change-Id: I775430d52dc2e91a6d0ad0df394245f34a8bb072

--- vars/Build.groovy
+++ vars/Build.groovy
@@ -12,12 +12,14 @@
  See the License for the specific language governing permissions and
  limitations under the License.*/
 
+
 import com.epam.edp.Codebase
 import com.epam.edp.Job
 import com.epam.edp.JobType
 import com.epam.edp.GitInfo
 import com.epam.edp.Nexus
 import com.epam.edp.Sonar
+import com.epam.edp.Chartmuseum
 import com.epam.edp.platform.PlatformType
 import com.epam.edp.platform.PlatformFactory
 import com.epam.edp.buildtool.BuildToolFactory
@@ -42,6 +44,12 @@ def call() {
 
             context.sonar = new Sonar(context.job, context.platform, this)
             context.sonar.init()
+
+            if (platform.checkObjectExists("route", "chartmuseum")) {
+               context.chartmuseum = new Chartmuseum(context.job, context.platform, this)
+               context.chartmuseum.init()
+               println("Chartmuseum init")
+            }
 
             context.codebase = new Codebase(context.job, context.git.project, context.platform, this)
             context.codebase.setConfig(context.git.autouser, context.git.host, context.git.sshPort, context.git.project,
