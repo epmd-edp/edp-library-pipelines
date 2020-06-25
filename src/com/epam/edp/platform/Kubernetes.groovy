d854bb1328d9a4ccc8e678376dfc1d33123b32bc
Viktor Semenov <Viktor_Semenov2@epam.com>
2020-06-25 12:06:29 +0300

    Add security check
    Change-Id: Ide0090dddb06ef2d289d57833cd421ae066aaf79

--- src/com/epam/edp/platform/Kubernetes.groovy
+++ src/com/epam/edp/platform/Kubernetes.groovy
@@ -181,4 +181,11 @@ class Kubernetes implements Platform {
         else
             script.println("[JENKINS][DEBUG] Rollback is not needed current status of ${name} is deployed")
     }
+
+    def runPod(project, image, name, options = null) {
+        script.sh(
+            script: "kubectl -n ${project} run --image=${image} --rm -i -t ${name} ${options}",
+            returnStdout: true
+        ).trim()
+    }
 }
